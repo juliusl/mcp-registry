3552cb93c4edd8b3f598c5d09a049c53c2550258
Julius <juliusl@microsoft.com>
2025-06-10 16:55:24 -0700

    feat: fix deploy.sh script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,6 +8,7 @@ registry_image=""
 registry_db_image=""
 env="test"
 secret_name=""
+namespace="io-github-mcp"
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -19,33 +20,69 @@ while [[ $# -gt 0 ]]; do
         secret_name="$2"
         shift 2
         ;;
+        --registry-image)
+        registry_image="$2"
+        shift 2
+        ;;
+        --db-image)
+        registry_db_image="$2"
+        shift 2
+        ;;
+        --namespace)
+        namespace="$2"
+        shift 2
+        ;;
         --*)
+        echo "Unrecognized flag: $1"
+        exit 1
+        ;;
     esac
 done
 
+if [[ -z "$registry_image" ]]; then
+echo "!!ERROR!!"
+echo
+echo "Value for --registry-image is required"
+echo "Example: myregistry.io/<mcp-registry-repo>:latest"
+exit 1
+fi
+
+if [[ -z "$registry_db_image" ]]; then
+echo "!!ERROR!!"
+echo
+echo "Value for --db-image is required"
+echo "Example: myregistry.io/mongo:latest"
+exit 1
+fi
+
 # Check if secret_name is set and output some useful information about the expectations
 if [[ -z "$secret_name" ]]; then
+    echo "!!ERROR!!"
+    echo
     echo "Value for --github-secret-name is required"
     echo "Expecting a secret to be deployed in the following format:"
     cat << EOF
+
 ---
 apiVersion: v1
 kind: Secret
 metadata:
   name: <your-secret-name-here>
-  namespace: io.github.mcp
+  namespace: $namespace
 type: Opaque
 data:
-  MCP_REGISTRY_GITHUB_CLIENT_SECRET: <base64 of 'MCP_REGISTRY_GITHUB_CLIENT_SECRET'>
-  MCP_REGISTRY_GITHUB_CLIENT_ID: <base64 of 'MCP_REGISTRY_GITHUB_CLIENT_ID'>
+  MCP_REGISTRY_GITHUB_CLIENT_SECRET:
+  MCP_REGISTRY_GITHUB_CLIENT_ID:
+
+
 EOF
     exit 1
 fi
 
 echo "Current template for mcp-registry deployment"
-helm template mcp-registry . \
-    -f ./deploy/values.yaml \
-    -f "./deploy/values.$env.yaml" \
+helm template . \
+    -f ./values.yaml \
+    -f "./values.$env.yaml" \
     --set registry.db="$registry_db" \
     --set registry.image="$registry_image" \
     --set db.mongo.image="$registry_db_image" \
@@ -53,9 +90,9 @@ helm template mcp-registry . \
     --debug
 
 echo "Deploying App"
-helm install mcp-registry . --create-namespace --namespace io.github.mcp \
-    -f ./deploy/values.yaml \
-    -f "./deploy/values.$env.yaml" \
+helm install . --generate-name --create-namespace --namespace "$namespace" \
+    -f ./values.yaml \
+    -f "./values.$env.yaml" \
     --set registry.db="$registry_db" \
     --set registry.image="$registry_image" \
     --set db.mongo.image="$registry_db_image" \
